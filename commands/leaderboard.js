5c3a40aa163b91b0c151b76a3280a4e693bcb51b
neriseo <82884420+Neiry6969@users.noreply.github.com>
2022-05-26 22:16:56 -0400

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -205,7 +205,7 @@ module.exports = {
 
         
 
-        const collector = leaderboard_msg.createMessageComponentCollector({ time: 120 * 1000 });
+        const collector = leaderboard_msg.createMessageComponentCollector({ time: 30 * 1000 });
 
         collector.on('collect', async (i) => {
             if(i.user.id != message.author.id) {
