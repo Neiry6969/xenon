49efa4f37ee911e159678383358f3d146cded39f
Chris Chen <chris.chen3128@gmail.com>
2022-05-29 15:10:52 -0400

    commit

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,6 +2,6 @@ module.exports = (Discord, client) => {
     console.log(`Logged in as ${client.user.tag}...`)
 
     client.user.setPresence({
-        activities: [{ name: `${client.guilds.cache.size} servers`, type: 'WATCHING' }],
+        activities: [{ name: `With you.`, type: 'PLAYING' }],
       })
 }
