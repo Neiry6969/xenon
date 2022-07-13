be09da357fbca60be9c7743c16076051a970f58d
Chris Chen <chris.chen3128@gmail.com>
2022-07-13 16:50:11 -0400

    this will never work

--- index.js
+++ index.js
@@ -54,21 +54,3 @@ process.on("unhandledRejection", (err) => {
     client.handleCommands(commandFolders, "./commands");
     client.login(discord_token);
 })();
-
-const express = require("express");
-const Topgg = require("@top-gg/sdk");
-
-const app = express();
-
-const webhook = new Topgg.Webhook(
-    "t3xA6XrFdjkV59VjneBMCZCH825KuXF2MHNfHStwS6smwCyR74"
-);
-
-app.post(
-    "/dblwebhook",
-    webhook.listener((vote) => {
-        console.log(vote.user);
-    })
-);
-
-app.listen(8080);
