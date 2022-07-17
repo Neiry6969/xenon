8614aeada7626e9971e4db6ad94abc25a1c1b053
neriseo <82884420+Neiry6969@users.noreply.github.com>
2022-07-16 23:09:13 -0400

    Update search_places.js

--- data/search_places.js
+++ data/search_places.js
@@ -1,166 +1,166 @@
 module.exports = [
-     {
-        place: 'god',
-        message: `You went to god and complained about life and he sent you away with ❀ \`COINS\`.`,
-        coins: 8000,
-        items: 'lifesaver',
-        itempecrent: 200,
-     },
-     {
-        place: 'big stone',
-        message: `Your curiousness led you to lifting the stone and you found ❀ \`COINS\`. Wow, I wonder who put it there?`,
-        coins: 4000,
-        items: 'finecoin',
-        itempecrent: 200,
-     },
-     {
-        place: 'store',
-        message: `You went in the store and walked out with ❀ \`COINS\`. Was this the power of coupons?`,
-        coins: 4000,
-        items: 'fineribbon',
-        itempecrent: 1000,
-     },
-     {
-        place: 'heaven',
-        message: `You stumbled because of your clumsiness and ended up in heaven. You didn't believe it, but you still found ❀ \`COINS\`! HEAVEN IS RICH!`,
-        coins: 10000,
-        items: 'chestofangelic',
-        itempecrent: 30,
-    },
-    {
-        place: 'random person',
-        message: `You patted the random person from head to toe and scavenged ❀ \`COINS\`. He was really confused, I think you should tell him.`,
-        coins: 10000,
-        items: 'airpods',
-        itempecrent: 59,
-    },
-    {
-        place: 'haunted mansion',
-        message: `While exploring because you were curious and had balls of steel, so you found ❀ \`COINS\` in a not shady room filled with odd shapped boxes. Huh? What was that? The door closed?`,
-        coins: 9000,
-        items: 'ghost',
-        itempecrent: 50,
-    },
-    {
-        place: 'communist farmland',
-        message: `You found ❀ \`COINS\`, I guess it is yours to keep now...`,
-        coins: 69,
-        items: 'potato',
-        itempecrent: 5696,
-    },
-    {
-        place: 'disney world',
-        message: `You found wallet that had ❀ \`COINS\`, who left their wallet here?`,
-        coins: 10000,
-        items: 'cellphone',
-        itempecrent: 3300,
-    },
-    {
-        place: 'local bank',
-        message: `You looked at the ATM machine so hard that it laughed out ❀ \`COINS\`. Are you a criminal now?`,
-        coins: 10000,
-        items: 'bankmessage',
-        itempecrent: 1000,
-    },
-    {
-        place: 'vector-tude fortress',
-        message: `You found ❀ \`COINS\` at vector's house. You better run because security is coming! BUT WHERE WAS THE BIG PRIZE!`,
-        coins: 15000,
-        items: 'moon',
-        itempecrent: 99,
-    },
-    {
-        place: 'studio',
-        message: `You found ❀ \`COINS\`, hopefuly this isn't stealling`,
-        coins: 10000,
-        items: 'acousticguitar',
-        itempecrent: 500,
-    },
-    {
-        place: 'beach',
-        message: `You searched the sandy beaches and found ❀ \`COINS\`, what a find!`,
-        coins: 5069,
-    },
-    {
-        place: 'valley',
-        message: `The valley had ❀ \`COINS\` and you just picked it up`,
-        coins: 2500,
-        items: 'painting',
-        itempecrent: 690,
-    },
-    {
-        place: 'alley',
-        message: `The alley had ❀ \`COINS\` and you just picked it up`,
-        coins: 1500,
-        items: 'scythe',
-        itempecrent: 100,
-    },
-    {
-        place: 'truck',
-        message: `There was a truck and it had ❀ \`COINS\``,
-        coins: 1500,
-        items: 'donut',
-        itempecrent: 9000,
-    },
-    {
-        place: 'school',
-        message: `You went to school and found ❀ \`COINS\``,
-        coins: 1000,
-    },
-    {
-        place: 'sewer',
-        message: `You went to sewers and savaged ❀ \`COINS\``,
-        coins: 6969,
-        items: 'waterbottle',
-        itempecrent: 7500,
-    },
-    {
-        place: 'glastonbury abbey',
-        message: `You looked around the ancient land and was blessed with ❀ \`COINS\``,
-        coins: 1000,
-        items: 'excaliburghost',
-        itempecrent: 1,
-    },
-    {
-        place: 'spring festival',
-        message: `You went to the beautiful festival and was given ❀ \`COINS\``,
-        coins: 9000,
-        items: 'purpleblossom',
-        itempecrent: 9800,
-    },
-    {
-        place: 'tree',
-        message: `You looked at a tree hoping it will give you money and it rained ❀ \`COINS\``,
-        coins: 15000,
-        items: 'goldenblossom',
-        itempecrent: 80,
-    },
-    {
-        place: 'grass',
-        message: `You touched some grass add got ❀ \`COINS\`, sheesh!`,
-        coins: 70,
-        items: 'blueblossom',
-        itempecrent: 1000,
-    },
-    {
-        place: 'skatepark',
-        message: `Random, nice stranger came and give you ❀ \`COINS\`, everyone is so nice these days!`,
-        coins: 10000,
-        items: 'skateboard',
-        itempecrent: 1000,
-    },
-    {
-        place: 'vamp empire',
-        message: `You went into the a scary castle and ran away with ❀ \`COINS\` as someone chased you.`,
-        coins: 10,
-        items: 'silvercrown',
-        itempecrent: 1,
-    },
-    {
-        place: 'vast field',
-        message: `You ran around a vast green field and found ❀ \`COINS\` truly remarkable.`,
-        coins: 10000,
-        items: 'greenblossom',
-        itempecrent: 1000,
-    },
-]
+  {
+    place: "god",
+    message: `You went to god and complained about life and he sent you away with \`❀ COINS\`.`,
+    coins: 8000,
+    items: "lifesaver",
+    itempecrent: 200
+  },
+  {
+    place: "big stone",
+    message: `Your curiousness led you to lifting the stone and you found \`❀ COINS\`. Wow, I wonder who put it there?`,
+    coins: 4000,
+    items: "finecoin",
+    itempecrent: 200
+  },
+  {
+    place: "store",
+    message: `You went in the store and walked out with \`❀ COINS\`. Was this the power of coupons?`,
+    coins: 4000,
+    items: "fineribbon",
+    itempecrent: 1000
+  },
+  {
+    place: "heaven",
+    message: `You stumbled because of your clumsiness and ended up in heaven. You didn't believe it, but you still found \`❀ COINS\`! HEAVEN IS RICH!`,
+    coins: 10000,
+    items: "chestofangelic",
+    itempecrent: 30
+  },
+  {
+    place: "random person",
+    message: `You patted the random person from head to toe and scavenged \`❀ COINS\`. He was really confused, I think you should tell him.`,
+    coins: 10000,
+    items: "airpods",
+    itempecrent: 59
+  },
+  {
+    place: "haunted mansion",
+    message: `While exploring because you were curious and had balls of steel, so you found \`❀ COINS\` in a not shady room filled with odd shapped boxes. Huh? What was that? The door closed?`,
+    coins: 9000,
+    items: "ghost",
+    itempecrent: 50
+  },
+  {
+    place: "communist farmland",
+    message: `You found \`❀ COINS\`, I guess it is yours to keep now...`,
+    coins: 69,
+    items: "potato",
+    itempecrent: 5696
+  },
+  {
+    place: "disney world",
+    message: `You found wallet that had \`❀ COINS\`, who left their wallet here?`,
+    coins: 10000,
+    items: "cellphone",
+    itempecrent: 3300
+  },
+  {
+    place: "local bank",
+    message: `You looked at the ATM machine so hard that it laughed out \`❀ COINS\`. Are you a criminal now?`,
+    coins: 10000,
+    items: "bankmessage",
+    itempecrent: 1000
+  },
+  {
+    place: "vector-tude fortress",
+    message: `You found \`❀ COINS\` at vector's house. You better run because security is coming! BUT WHERE WAS THE BIG PRIZE!`,
+    coins: 15000,
+    items: "moon",
+    itempecrent: 99
+  },
+  {
+    place: "studio",
+    message: `You found \`❀ COINS\`, hopefuly this isn't stealling`,
+    coins: 10000,
+    items: "acousticguitar",
+    itempecrent: 500
+  },
+  {
+    place: "beach",
+    message: `You searched the sandy beaches and found \`❀ COINS\`, what a find!`,
+    coins: 5069
+  },
+  {
+    place: "valley",
+    message: `The valley had \`❀ COINS\` and you just picked it up`,
+    coins: 2500,
+    items: "painting",
+    itempecrent: 690
+  },
+  {
+    place: "alley",
+    message: `The alley had \`❀ COINS\` and you just picked it up`,
+    coins: 1500,
+    items: "scythe",
+    itempecrent: 100
+  },
+  {
+    place: "truck",
+    message: `There was a truck and it had \`❀ COINS\``,
+    coins: 1500,
+    items: "donut",
+    itempecrent: 9000
+  },
+  {
+    place: "school",
+    message: `You went to school and found \`❀ COINS\``,
+    coins: 1000
+  },
+  {
+    place: "sewer",
+    message: `You went to sewers and savaged \`❀ COINS\``,
+    coins: 6969,
+    items: "waterbottle",
+    itempecrent: 7500
+  },
+  {
+    place: "glastonbury abbey",
+    message: `You looked around the ancient land and was blessed with \`❀ COINS\``,
+    coins: 1000,
+    items: "excaliburghost",
+    itempecrent: 1
+  },
+  {
+    place: "spring festival",
+    message: `You went to the beautiful festival and was given \`❀ COINS\``,
+    coins: 9000,
+    items: "purpleblossom",
+    itempecrent: 9800
+  },
+  {
+    place: "tree",
+    message: `You looked at a tree hoping it will give you money and it rained \`❀ COINS\``,
+    coins: 15000,
+    items: "goldenblossom",
+    itempecrent: 80
+  },
+  {
+    place: "grass",
+    message: `You touched some grass add got \`❀ COINS\`, sheesh!`,
+    coins: 70,
+    items: "blueblossom",
+    itempecrent: 1000
+  },
+  {
+    place: "skatepark",
+    message: `Random, nice stranger came and give you \`❀ COINS\`, everyone is so nice these days!`,
+    coins: 10000,
+    items: "skateboard",
+    itempecrent: 1000
+  },
+  {
+    place: "vamp empire",
+    message: `You went into the a scary castle and ran away with \`❀ COINS\` as someone chased you.`,
+    coins: 10,
+    items: "silvercrown",
+    itempecrent: 1
+  },
+  {
+    place: "vast field",
+    message: `You ran around a vast green field and found \`❀ COINS\` truly remarkable.`,
+    coins: 10000,
+    items: "greenblossom",
+    itempecrent: 1000
+  }
+];
