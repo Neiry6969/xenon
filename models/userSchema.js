485cdfbec73e1400a5b3db2421dd3d9625a15d51
neriseo <82884420+Neiry6969@users.noreply.github.com>
2022-08-14 22:40:56 -0400

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -8,99 +8,11 @@ const userSchema = new mongoose.Schema(
             unique: true,
         },
 
-        job: {
-            type: Object,
-            default: {
-                name: null,
-                totalhoursworked: 0,
-                currentjobhoursworked: 0,
-                streak: 0,
-                lastworked: null,
-                hoursworkedday: 0,
-            },
-        },
-        showcase: {
-            type: Object,
-            default: {
-                slots: 0,
-                items: {
-                    type: Object,
-                    default: {},
-                },
-            },
-        },
-        pet: {
-            type: Object,
-            default: {
-                species: null,
-                name: null,
-                hygiene: {
-                    percent: null,
-                    lastinteract: null,
-                },
-                hunger: {
-                    percent: null,
-                    lastinteract: null,
-                },
-                experience: {
-                    percent: null,
-                    lastinteract: null,
-                },
-                toys: {
-                    type: Object,
-                    default: {},
-                },
-            },
-        },
-        moderation: {
-            type: Object,
-            default: {
-                blacklist: {
-                    date: null,
-                    status: false,
-                    reason: null,
-                    unblacklistdate: null,
-                    resposiblemod: null,
-                },
-                ban: {
-                    date: null,
-                    status: false,
-                    reason: null,
-                    unblacklistdate: null,
-                    resposiblemod: null,
-                },
-                logs: {
-                    type: Array,
-                    default: [],
-                },
-            },
-        },
-        knowledge: {
-            type: Number,
-            default: 0,
-        },
         settings: {
             type: Object,
             default: {},
         },
-        usersocial: {
-            type: Object,
-            default: {
-                thumbnailurl: "",
-                followers: {
-                    type: Array,
-                    default: [],
-                },
-                following: {
-                    type: Array,
-                    default: [],
-                },
-                desc: {
-                    type: String,
-                    default: "",
-                },
-            },
-        },
+        
         activeitems: {
             type: Object,
             default: {},
